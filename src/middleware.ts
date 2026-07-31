5d7c9c15247071afc2072f142156518e62e03587
Taemin97 <xherdanpark97@gmail.com>
2026-08-01 03:42:29 +0900

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+    locales: ['ko', 'en', 'fr'],
+    defaultLocale: 'ko',
+    localePrefix: 'as-needed'
+});
+
+export const config = {
+    matcher: ['/', '/(ko|en|fr)/:path*', '/((?!_next|_vercel|.*\\..*).*)']
+}
