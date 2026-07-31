5e22fbf9cc2967d1d2b481079cf71c45107840e3
Taemin97 <xherdanpark97@gmail.com>
2026-08-01 03:46:04 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// next-intl 플러그인 초기화 (request.ts 경로 명시)
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
