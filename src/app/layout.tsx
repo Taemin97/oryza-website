8a8e9efcc8df8a9954c4b1bb15a43a727053d53b
Taemin97 <xherdanpark97@gmail.com>
2026-08-01 13:45:30 +0900

    fix: next-intl v4 middleware + remove duplicate html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="ko" suppressHydrationWarning>
-      <body>{children}</body>
-    </html>
-  );
+  return children;
 }
